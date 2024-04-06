390ffbb2d5589bd94031b184a614c5ed28aa5a7a
Moon Chae Geon <mooncg0916@gmail.com>
2024-04-06 23:51:28 +0900

    book, bookstore class with basic function

--- Bookstore/main.cpp
+++ Bookstore/main.cpp
@@ -1,32 +1,60 @@
 #include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
 
-class Sales_item {
-    int isbn;
-    int count;
-    int price;
+using namespace std;
 
+class Book {
+private:
+	string title;
+	string author;
+	string isbn;
+	int price, quantity, published_year;
 public:
-    Sales_item(int isbn, int count, int price) : isbn(isbn), count(count), price(price) {}
+	Book(string _title, string _author, string _isbn, int _price, int _quantity, int _published_year)
+		: title(_title), author(_author), isbn(_isbn), price(_price), quantity(_quantity), published_year(_published_year) {}
 
-    friend std::ostream& operator<<(std::ostream& os, const Sales_item& sales_item) {
-        os << sales_item.isbn << " " << sales_item.count << " " << sales_item.price;
-        return os;
-    }
+	string getISBN() {
+		return isbn;
+	}
 
-    friend std::istream& operator>>(std::istream& input, Sales_item& sales_item) {
-        input >> sales_item.isbn >> sales_item.count >> sales_item.price;
-        return input;
-    }
 
+	void showInfo() {
+		cout << title << author << isbn << price << quantity << endl;
+	}
+};
+
+class Bookstore {
+	vector<Book> books;
+public:
+	Bookstore() {}
+
+	void addBook(const Book & book) {
+		books.push_back(book);
+	}
+
+	void removeBook(const string& isbn) {
+		for (auto it = books.begin(); it != books.end(); ++it) {
+			if (it->getISBN() == isbn) {
+				books.erase(it);
+				break;
+			}
+		}
+	}
+
+	void printBooks() {
+		for (auto& book : books) {
+			book.showInfo();
+		}
+	}
 };
 
 int main() {
-    Sales_item item(123456, 5, 20);
-    std::cout << item << std::endl;
-
-    int cnt = 0;
-    while (std::cin >> item) {
-        ++cnt;
-    }
-    std::cout << "Count: " << cnt << std::endl;
+	Book book1("John Wick", "Brothers", "4444444", 10000, 100, 2023);
+	Bookstore First_bookstore;
+	First_bookstore.addBook(book1);
+	First_bookstore.printBooks();
 }
